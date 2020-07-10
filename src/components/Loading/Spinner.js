327d4e90bcaaf8a52efbdccb440db7278c6bb5a3
Patrick Huber <patrick.huber1@gmail.com>
2020-07-10 09:37:49 -0500

    Added loading spinner to find location button and zoom in when location found

---
+++ src/components/Loading/Spinner.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { css } from "@emotion/core";
+import ClipLoader from "react-spinners/ClipLoader";
+ 
+const override = css`
+  display: inline-block;
+  border-color: red;
+`;
+ 
+export default class Spinner extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
+ 
+  render() {
+    return (
+      <div className="spinner">
+        <ClipLoader
+          css={override}
+          size={30}
+          color={"#000000"}
+          loading={this.state.loading}
+        />
+      </div>
+    );
+  }
+}
--- src/components/Map/GMap.js
+++ src/components/Map/GMap.js
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom';
 
 import { GoogleMap, LoadScript, InfoWindow, Marker } from '@react-google-maps/api';
 
+import { Spinner } from '../Loading';
+
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 
-// const [selectedVendor, setSelectedVendor] = useState(null);
-
-const mapContainerStyle = {
-  height: '100vh',
-  width: '100vw',
+const mapOptions = {
+  mapContainerStyle : {
+    height: '100vh',
+    width: '100vw',
+  },
+  center: {
+    lat: 44.9778,
+    lng: -93.2650
+  },
+  zoom: 11,
 }
-
-const center = {
-  lat: 44.9778,
-  lng: -93.2650
-}
-
-const position = { lat: 44.9422948, lng: -93.3288194 }
 
 const divStyle = {
   background: `white`,
@@ -35,6 +35,7 @@ class GMap extends Component {
       loading: false,
       selected: null,
       mapRef: null,
+      locationLoading: false,
     };
   }
 
@@ -77,34 +78,50 @@ class GMap extends Component {
   }
 
   render() {
-    const { calendar, loading, selected, mapRef } = this.state;
-
+    const { calendar, loading, locationLoading, selected, mapRef } = this.state;
 
     return (
       <div>
         <button
           className="locate"
           onClick={() => {
+            this.setState({
+              locationLoading: true,
+            })
             navigator.geolocation.getCurrentPosition(
               (position) => {
+                const newZoom = mapOptions.zoom + 2;
+                this.setState({
+                  locationLoading: false,
+                });
                 mapRef.panTo({
                   lat: position.coords.latitude,
                   lng: position.coords.longitude,
                 });
+                mapRef.setZoom(newZoom);
               },
-              () => null
+              () => {
+                this.setState({
+                  locationLoading: false,
+                })
+                alert('We were unable to find your current location. Please try searching for a location.');
+              }
             );
           }}
-        >Find My Location
+        >
+          Find My Location
+          {locationLoading &&
+            <Spinner />
+          }
         </button>
         <LoadScript
           googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API}
         >
           <GoogleMap
             id="Food-Finder-Map"
-            mapContainerStyle={mapContainerStyle}
-            zoom={11}
-            center={center}
+            mapContainerStyle={mapOptions.mapContainerStyle}
+            zoom={mapOptions.zoom}
+            center={mapOptions.center}
             onClick={() => {
               this.setSelected(null);
             }}
@@ -121,7 +138,7 @@ class GMap extends Component {
             ))}
             {selected ? (
               <InfoWindow
-                position={{ lat: 44.950575, lng: -93.320129 }}
+                position={{ lat: selected.location.latitude, lng: selected.location.longitude }}
                 options={{pixelOffset: new window.google.maps.Size(0,-40)}}
                 onCloseClick ={() => {
                   this.setSelected(null);
